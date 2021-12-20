f6d4df5dc15e2782a6a23154d69abdfcce29d0c9
tehZevo <tehZevo@users.noreply.github.com>
2021-12-19 19:56:40 -0500

    Imagine phoneposting (Remove TODO)

--- download.py
+++ download.py
@@ -1,6 +1,5 @@
 import os
 from aitextgen import aitextgen
 
-#TODO: support other models such as "minimaxir/hacker-news" or "EleutherAI/gpt-neo-125M" via model parameter
 MODEL = os.getenv("MODEL", None)
 ai = aitextgen(model=MODEL)
